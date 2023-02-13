6aa9f34a9125ad5932ae02fe2cb52e20c1bab70a
nkc-iwamoto <ayato20030213@icloud.com>
2023-02-13 13:56:59 +0900

    update:Reflector.cs

--- ReflectBeam_Prot/Assets/Iwas/Reflector.cs
+++ ReflectBeam_Prot/Assets/Iwas/Reflector.cs
@@ -3,9 +3,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Reflector : MonoBehaviour,IGimmick
+public class Reflector : MonoBehaviour, IGimmick
 {
     public MeshFilter mf;
+
+    public GameObject g;
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        Vector3 vec = g.transform.position - transform.position;
+        Ray ray = new Ray(transform.position, vec);
+        Debug.DrawRay(transform.position, vec, Color.black);
+        OnHit(vec);
+    }
 
     // ギミックのUIサイズ
     GimmickUISize IGimmick.GetSize()
@@ -13,8 +27,14 @@ public class Reflector : MonoBehaviour,IGimmick
         return new GimmickUISize(new Vector2(0, 0), new Vector2(3, 3), Vector2.one);
     }
     // 当たったとき
-    public Vector3 OnHit()
+    public Vector3 OnHit(Vector3 vec)
     {
-       return Vector3.forward;
+        Vector3 forward = transform.forward;
+        // 外積
+        Vector3 result = Vector3.Cross(forward, vec).normalized;
+        // レイを可視化
+        Debug.DrawRay(transform.position, result, Color.blue);
+
+        return Vector3.zero;
     }
 }
