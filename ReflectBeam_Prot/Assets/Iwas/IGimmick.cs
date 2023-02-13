69610c9671ce9de3c205ff56fe46f6f70d532c6a
nkc-iwamoto <ayato20030213@icloud.com>
2023-02-13 13:56:32 +0900

    update:Reflecotr.cs

--- ReflectBeam_Prot/Assets/Iwas/IGimmick.cs
+++ ReflectBeam_Prot/Assets/Iwas/IGimmick.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 interface IGimmick
 {
     // ギミックが当たった時に動く処理
-    public Vector3 OnHit();
+    public Vector3 OnHit(Vector3 vec);
 
     // ギミックのサイズを取得する処理
     public GimmickUISize GetSize();
