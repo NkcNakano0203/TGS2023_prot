6a3da86b68d4c4326a09438641d6a2eb455e655e
IchinoseYuki <nkc.yuki.ichinose@gmail.com>
2023-02-13 10:59:09 +0900

    add:市瀬フォルダー + コントローラースクリプト

---
+++ ReflectBeam_Prot/Assets/IchinoseYuki/Script/RemoteControl/RemoteControl.cs
@@ -0,0 +1,18 @@
+// 作成日02/13日 製作者:市瀬
+using UnityEngine;
+
+/// <summary>
+/// リモコンスクリプト
+/// </summary>
+public class RemoteControl : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
