360f317e7e7653d252b8cc0074129abed6649a70
nkc-iwamoto <ayato20030213@icloud.com>
2023-02-14 12:05:37 +0900

    add:自由回転,固定回転のスクリプト

---
+++ ReflectBeam_Prot/Assets/Iwas/FixedRotation.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixedRotation : MonoBehaviour, IRotatable
+{
+    public void LeftRotate()
+    {
+        
+    }
+
+    public void RightRotate()
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ ReflectBeam_Prot/Assets/Iwas/FreeRotation.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreeRotation : MonoBehaviour,IRotatable
+{
+    public void LeftRotate()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void RightRotate()
+    {
+        throw new System.NotImplementedException();
+    }
+}
