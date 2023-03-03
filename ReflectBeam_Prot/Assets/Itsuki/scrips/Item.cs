58ce00cc88d761f650eaf9e339a8f2f5b292d91c
IchinoseYuki <nkc.yuki.ichinose@gmail.com>
2023-03-03 14:22:39 +0900

    Update:ギア更新

--- ReflectBeam_Prot/Assets/Itsuki/scrips/Item.cs
+++ ReflectBeam_Prot/Assets/Itsuki/scrips/Item.cs
@@ -1,11 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Item : MonoBehaviour
 {
     private void Update()
     {
-        transform.Rotate(0,3f,0);
+        transform.Rotate(0, 1.5f, 0);
     }
 }
